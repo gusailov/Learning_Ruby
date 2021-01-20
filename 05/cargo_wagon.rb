f4d1c2bf85400411135fed053ef310210b6066d9
gusailov <gusailov@gmail.com>
2021-01-20 23:14:52 +0200

    wagons_fin

--- 05/cargo_wagon.rb
+++ 05/cargo_wagon.rb
@@ -1,3 +1,29 @@
 class CargoWagon
   include CompanyName
+  attr_reader :total_volume
+
+  def initialize(volume)
+    @total_volume = volume
+    @occupied_volume = 0
+    @available_volume = volume
+  end
+
+  def fill_volume(vol)
+    if @available_volume.zero?
+      puts 'Весь объем занят'
+    elsif @available_volume < vol
+      puts "В вагоне осталось только '#{@available_volume}' свободного объема"
+    else
+      @available_volume -= vol
+      @occupied_volume += vol
+    end
+  end
+
+  def occupied_volume_qty
+    @occupied_volume
+  end
+
+  def available_volume_qty
+    @available_volume
+  end
 end
--- 05/passenger_wagon.rb
+++ 05/passenger_wagon.rb
@@ -1,3 +1,28 @@
 class PassengerWagon
   include CompanyName
+
+  attr_reader :all_seats
+
+  def initialize(seats)
+    @all_seats = seats
+    @occupied_seats = 0
+    @free_seats = seats
+  end
+
+  def take_seat
+    if @free_seats.zero?
+      puts 'Свободных мест нет'
+    else
+      @free_seats -= 1
+      @occupied_seats += 1
+    end
+  end
+
+  def occupied_seats_num
+    @occupied_seats
+  end
+
+  def free_seats_num
+    @free_seats
+  end
 end
