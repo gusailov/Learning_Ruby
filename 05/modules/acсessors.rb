7f16fb0f89f67355de664684d856bbc85f4ed55a
gusailov <gusailov@gmail.com>
2021-01-28 18:09:16 +0200

    pause

--- 05/modules/acсessors.rb
+++ 05/modules/acсessors.rb
@@ -6,18 +6,21 @@ module Acсessors
 
   module ClassMethods
     def attr_accessor_with_history(*names)
-      values = []
       names.each do |name|
         var_name = "@#{name}".to_sym
         define_method(name) { instance_variable_get(var_name) }
         define_method("#{name}=".to_sym) do |value|
+          @values = []
           instance_variable_set(var_name, value)
-          values << value
+          @values << value
         end
       end
     end
   end
 
   module InstanceMethods
+    def values
+      @values ||= []
+    end
   end
 end
